fb2029642e7858038496efb6130e753d2351e4a3
Tiago <zorondoemmanuel@gmail.com>
2022-08-31 14:31:54 -0300

    clase05 completada

--- components/checkComplete.js
+++ components/checkComplete.js
@@ -1,17 +1,22 @@
-const checkComplete = () =>{
-    const i = document.createElement('i');
-    i.classList.add('far','fa-check-square', 'icon');
-    i.addEventListener("click", completeTask);
-    return i;
-}
+const checkComplete = (id) => {
+  const i = document.createElement("i");
+  i.classList.add("far", "fa-check-square", "icon");
+  i.addEventListener("click", (e) => completeTask(e, id));
+  return i;
+};
 
 // Inmediately invoked function expression  IIFE
 
-const completeTask = (event) => {
-  const element = event.target;
+const completeTask = (e, id) => {
+  const element = e.target;
   element.classList.toggle("fas");
-  element.classList.toggle("far");
   element.classList.toggle("completeIcon");
+  element.classList.toggle("far");
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  const index = tasks.findIndex((item) => item.id == id);
+  console.log(index);
+  tasks[index]["complete"] = !tasks[index]["complete"];
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
 export default checkComplete;
--- script.js
+++ script.js
@@ -1,29 +1,9 @@
-import checkComplete from './components/checkComplete.js';
-import deleteIcon from './components/deletIcon.js';
+import { addTask } from "./components/addTask.js";
+import { displayTasks } from "./components/readTasks.js";
 
 const btn = document.querySelector("[data-form-btn]");
 
-
-const creatTask = (e) => {
-    e.preventDefault();
-    const input = document.querySelector("[data-form-input]");
-    const value = input.value;
-    const list = document.querySelector("[data-list]");
-    const task = document.createElement('li');
-    const titleTask = document.createElement("span");
-    const taskContent = document.createElement("div");
-    task.classList.add("card");
-    titleTask.classList.add("task");
-    input.value = '';
-    list.appendChild(task);
-    task.appendChild(taskContent);
-    taskContent.appendChild(checkComplete());
-    taskContent.appendChild(titleTask);
-    task.appendChild(deleteIcon());
-
-    titleTask.innerText = value;
-};
-
 // arrow functions o funciones anonimas
-btn.addEventListener("click", creatTask);
+btn.addEventListener("click", addTask);
 
+displayTasks();
